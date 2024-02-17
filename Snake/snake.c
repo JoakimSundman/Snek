1c6d3a88c5708515229b5c5665d757116ec95a13
JoakimSundman <itlock993@gmail.com>
2024-02-17 22:10:52 +0100

    Some changes with init and testing a board

--- Snake/snake.c
+++ Snake/snake.c
@@ -39,8 +39,7 @@ void user_isr( void )
   } 
 
   if(timeoutcounter == 10){
-    time2string( textstring, mytime );
-    display_string( 3, textstring );
+    display_image(0, board);
     display_update();
     timeoutcounter = 0;
   }
@@ -57,14 +56,20 @@ void labinit( void )
   volatile unsigned *trisEPointer = (unsigned int *) 0xbf886100; // volatile pointer that points to TRISE in memory
   *trisEPointer = 0x00; // volatile pointer sets bits 0 through 7 to outputs                     handles LEDS
 
+  PORTE = 0x0000; // sends 0 to port E 
+
   TRISD |= 0x07F0; // Sets bits 5 to 11 to inputs, bitwise or |= leaves other bits unchanged, 8-11 SWITCHES, 5-7 BTNS
 
-  T2CON = 0x0000; // Reset configuration of timer 2 and turning it off 
-  T2CONSET = 0x70; // sets prescaling to 1/256
-  PR2 = 80000000/(256*10); // how many periods the timer counts     divides by 10 due to seconds/10 
+  T2CON = 0x0000; // Reset configuration of timer 2 and turning it off
+  T2CONSET = 0x70; // sets prescaling to 1/256 
+  PR2 = 80000000/(256*10); // how many periods the timer counts     divides by 10 due to seconds/10
   TMR2 = 0x0; // clears the counter value of timer 2 
-  IFSCLR(0) = 0x0; // clears the interrupt flag status 
-  T2CONSET = 0x8000; // sets bit 15 to 1 stating the clock 
+  IFSCLR(0) = 0x0000; // clears the interrupt flag status
+  T2CONSET = 0x8000; // sets bit 15 to 1 stating the clock
+  IECSET(0) = 0x100;
+  IECSET(0) = 0x8000;           // Sets bit 16 to initialize 
+  IPCSET(2) = 0x1F;
+  enable_interrupt();
   return;
 }
 
